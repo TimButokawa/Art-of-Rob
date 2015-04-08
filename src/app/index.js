92d3f8719fa84872c3e9aa3000719a39a697ae83
timbutokawa <tim@timbutorac.com>
2015-04-08 08:10:06 -0700

    begin projects directive - add routes

--- src/app/index.js
+++ src/app/index.js
@@ -4,10 +4,34 @@
     angular.module('robBrown', ['ui.router', 'ui.bootstrap'])
         .config(function ($stateProvider, $urlRouterProvider) {
             $stateProvider
-                .state('home', {
+                .state('projects', {
                     url: '/',
-                    templateUrl: 'app/main/main.html',
-                    controller: 'Main as main'
+                    templateUrl: 'app/projects/projects.html',
+                    controller: 'Projects as project'
+                })
+                .state('works', {
+                    url: '/works',
+                    templateUrl: 'app/works/works.html'
+                })
+                .state('lab', {
+                    url: '/lab',
+                    templateUrl: 'app/lab/lab.html'
+                })
+                .state('about', {
+                    url: '/about',
+                    templateUrl: 'app/about/about.html'
+                })
+                .state('cv', {
+                    url: '/cv',
+                    templateUrl: 'app/cv/cv.html'
+                })
+                .state('contact', {
+                    url: '/contact',
+                    templateUrl: 'app/contact/contact.html'
+                })
+                .state('links', {
+                    url: '/links',
+                    templateUrl: 'app/links/links.html'
                 });
 
             $urlRouterProvider.otherwise('/');
