f11bab30730242a6cfabba34f826688cbbdaeb57
timbutokawa <tim@timbutorac.com>
2015-04-08 21:19:46 -0700

    rename view

---
+++ src/app/projects/projects.controller.js
@@ -0,0 +1,10 @@
+(function() {
+  'use strict';
+
+    angular.module('robBrown').controller('Projects', Projects);
+
+    function Projects() {
+        var vm = this;
+        vm.title = 'rob brown';
+    }
+})();
